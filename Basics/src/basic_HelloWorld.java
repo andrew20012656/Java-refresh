e22b2cb9ff58a577e26d87b71d16e833658d47ac
andrew20012656 <andrew20012656@gmail.com>
2022-07-14 23:41:25 -0400

    integer type

--- Basics/src/basic_HelloWorld.java
+++ Basics/src/basic_HelloWorld.java
@@ -5,6 +5,16 @@ public class basic_HelloWorld {
 		System.out.println("Hello World!");
 		
 		int firstNum = 1;
+		
+		int smallestNum = Integer.MIN_VALUE;
+		int maxNum = Integer.MAX_VALUE;
+		System.out.println("Smallest integer: " + smallestNum);
+		System.out.println("Largest integer: " + maxNum);
+		
+		// Overflow and Underflow
+		System.out.println("Overflow: " + (maxNum + 1));
+		System.out.println("Underflow: " + (smallestNum - 1));
+		
 	}
 
 }
